91785fd1a2a7bee1c317e144f281ac3e142f5971
Simon Gravelle <simon.gravelle@live.fr>
2024-07-28 13:52:18 +0200

    improved input

--- bulk-phases/H2O-spce/launch-GOMC.sh
+++ bulk-phases/H2O-spce/launch-GOMC.sh
@@ -2,18 +2,43 @@
 
 set -e
 
+# link to LAMMPS
+lmp=/home/simon/Softwares/LAMMPS-GUI-1.6.3/lmp
+
 # Choose the chemical potential
 for mu in 3000 3200 3400 3600 3800 4000 4200 4400 4600
 do
 
+    if [[ $mu -gt 3300 ]]
+    then
+        # expected vapor
+        box0=200
+    else
+        # expected liquid
+        box0=30
+    fi
+    echo "Chemical potential = -"${mu}" K --- Box size = "${box0}" A"
+
+    # create folder for data saving
     data_folder='outputs_mu'${mu}
     mkdir -p ${data_folder}
 
+    # Adjust initial box size
+    # Call LAMMPS
+    cd topology/lammps
+        newline='variable box0 equal '${box0}
+        oldline=$(cat input.lmp| grep 'variable box0 equal ')  
+        sed -i '/'"$oldline"'/c\'"$newline" input.lmp
+        ${lmp} -in input.lmp
+    cd ../../
+
+    # Replace the chemical potential value in the input
     newline='ChemPot H2O -'${mu}
     oldline=$(cat input.gomc| grep 'ChemPot H2O')
     sed -i '/'"$oldline"'/c\'"$newline" input.gomc
 
-    # Ensure that the volume in the input file is the right one
+    # Ensure that the volume in the input file is consistent
+    # with the initial topology
     cp ../../scripts/adjust_volume.py .
     python3 adjust_volume.py
     rm adjust_volume.py
@@ -31,6 +56,8 @@ do
 
     mv output_* ${data_folder}
     mv *.dat ${data_folder}
+
+
 
 done
 
