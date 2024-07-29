88f6c69f68d0060b75805bbc835b8ad4b826f175
Simon Gravelle <simon.gravelle@live.fr>
2024-07-29 22:29:50 +0200

    added more gcmc results

---
+++ bulk-phases/SPCE/lammps-comparison/launch-LAMMPS.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -e
+
+# link to LAMMPS
+LMP=/home/simon/Softwares/LAMMPS-GUI-1.6.4/lmp
+
+# Choose the chemical potential
+for mu in {2800..5000..200}
+do
+
+    Nstep=1000000
+    if [[ $mu -gt 3900 ]]
+    then
+        # expected vapor
+        box0=50
+        Nattempt=10
+        Nb0=50
+    else
+        # expected liquid
+        box0=25
+        Nattempt=100
+        Nb0=1000
+    fi
+    echo "Chemical potential = -"${mu}" K --- Box size = "${box0}" A"
+
+    # Adjust initial box size
+    # Call LAMMPS
+    cd ../topology/lammps
+        newline='variable Nb0 equal '${Nb0}
+        oldline=$(cat input.lmp| grep 'variable Nb0 equal')  
+        sed -i '/'"$oldline"'/c\'"$newline" input.lmp
+        newline='variable box0 equal '${box0}
+        oldline=$(cat input.lmp| grep 'variable box0 equal')  
+        sed -i '/'"$oldline"'/c\'"$newline" input.lmp
+        ${LMP} -in input.lmp
+    cd ../../lammps-comparison/
+
+    cp ../topology/lammps/box.data .
+    cp ../topology/lammps/parm.lmp .
+    cp ../topology/lammps/H2O.mol .
+
+    # Replace the chemical potential value in the input
+    newline='variable mu_K equal -'${mu}
+    oldline=$(cat input.lmp| grep 'variable mu_K equal')
+    sed -i '/'"$oldline"'/c\'"$newline" input.lmp
+
+    newline='variable Nstep equal '${Nstep}
+    oldline=$(cat input.lmp| grep 'variable Nstep equal')
+    sed -i '/'"$oldline"'/c\'"$newline" input.lmp
+
+    newline='variable Nattempt equal '${Nattempt}
+    oldline=$(cat input.lmp| grep 'variable Nattempt equal')
+    sed -i '/'"$oldline"'/c\'"$newline" input.lmp
+    
+    OMP_NUM_THREADS=8 ${LMP} -in input.lmp -sf omp
+
+    # create folder for data saving
+    data_folder='outputs_mu'${mu}
+    mkdir -p ${data_folder}
+
+    mv *.dat ${data_folder}
+    mv log.lammps ${data_folder}
+
+    rm parm.lmp
+    rm box.data
+    rm H2O.mol
+
+done
---
+++ bulk-phases/SPCE/launch-GOMC.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+set -e
+
+# link to LAMMPS
+LMP=/home/simon/Softwares/LAMMPS-GUI-1.6.4/lmp
+# link to GOMC
+GOMC=/home/simon/Softwares/GOMC/bin/GOMC_CPU_GCMC
+
+# Choose the chemical potential
+for mu in {4600..5000..200}
+do
+
+    Nstep=2500000
+    NCoord=10000
+    if [[ $mu -gt 4700 ]]
+    then
+        # expected vapor
+        box0=50
+        Nb0=50
+        Nb1=2000
+    else
+        # expected liquid
+        box0=25
+        Nb0=1000
+        Nb1=2000
+    fi
+    echo "Chemical potential = -"${mu}" K --- Box size = "${box0}" A"
+
+    # Adjust initial box size
+    # Call LAMMPS
+    cd topology/lammps
+        newline='variable box0 equal '${box0}
+        oldline=$(cat input.lmp| grep 'variable box0 equal ')  
+        sed -i '/'"$oldline"'/c\'"$newline" input.lmp
+        newline='variable Nb1 equal '${Nb1}
+        oldline=$(cat input.lmp| grep 'variable Nb1 equal ')  
+        sed -i '/'"$oldline"'/c\'"$newline" input.lmp
+        newline='variable Nb0 equal '${Nb0}
+        oldline=$(cat input.lmp| grep 'variable Nb0 equal ')
+        sed -i '/'"$oldline"'/c\'"$newline" input.lmp
+        ${LMP} -in input.lmp
+    cd ../../
+
+    # Two possibilities: impose the chemical potential directly, or impose the pressure
+
+    # estimate pressure and replace in the input
+    python3 pressure_from_chemical_potential.py "$mu"
+    # Replace the chemical potential value in the input
+    #newline='ChemPot CO2 -'${mu}
+    #oldline=$(cat input.gomc| grep 'ChemPot CO2')
+    #sed -i '/'"$oldline"'/c\'"$newline" input.gomc
+
+    newline='RunSteps '${Nstep}
+    oldline=$(cat input.gomc| grep 'RunSteps')
+    sed -i '/'"$oldline"'/c\'"$newline" input.gomc
+
+    newline='CoordinatesFreq true '${NCoord}
+    oldline=$(cat input.gomc| grep 'CoordinatesFreq true')
+    sed -i '/'"$oldline"'/c\'"$newline" input.gomc
+
+    # Ensure that the volume in the input file is consistent
+    # with the initial topology
+    cp ../../scripts/adjust_volume.py .
+    python3 adjust_volume.py
+    rm adjust_volume.py
+
+    # Create the GOMC topology from LAMMPS files
+    cd topology/
+    cp ../../../scripts/create_initial_configuration.py .
+    python3 create_initial_configuration.py
+    rm create_initial_configuration.py
+    cd ..
+
+    ${GOMC} +p8 input.gomc > log.gomc
+
+    # create folder for data saving
+    data_folder='outputs_mu'${mu}
+    mkdir -p ${data_folder}
+
+    mv output_* ${data_folder}
+    mv *.dat ${data_folder}
+    mv log.gomc ${data_folder}
+
+done
+
+cp ../../scripts/analysis.py .
+python3 analysis.py
+rm analysis.py
