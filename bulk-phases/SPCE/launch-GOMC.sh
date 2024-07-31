3d083a17f8d221aff566c645c4988188aa1c8aa7
Simon Gravelle <simon.gravelle@live.fr>
2024-07-31 21:18:31 +0200

    added bulk SPCE results

--- bulk-phases/SPCE/launch-GOMC.sh
+++ bulk-phases/SPCE/launch-GOMC.sh
@@ -8,14 +8,14 @@ LMP=/home/simon/Softwares/LAMMPS-GUI-1.6.4/lmp
 GOMC=/home/simon/Softwares/GOMC/bin/GOMC_CPU_GCMC
 
 # Choose the chemical potential
-for mu in {4300..5200..100}
+for mu in {4800..5200..100}
 do
 
     Nstep=2500000
-    NCoord=100
+    NCoord=10000
     Nb0=0
-    Nb1=1600
-    if [[ $mu -gt 4500 ]]
+    Nb1=3000
+    if [[ $mu -gt 4850 ]]
     then
         # expected vapor
         box0=80
@@ -72,16 +72,16 @@ do
 
     ${GOMC} +p8 input.gomc > log.gomc
 
+    cp ../../scripts/analysis_single_folder.py .
+    python3 analysis_single_folder.py
+    rm analysis_single_folder.py
+    
     # create folder for data saving
-    data_folder='outputs_mu'${mu}
+    data_folder='outputs_mu'${mu}'/'
     mkdir -p ${data_folder}
 
-    mv output_* ${data_folder}
     mv *.dat ${data_folder}
     mv log.gomc ${data_folder}
-
-    cp ../../scripts/analysis.py .
-    python3 analysis.py
-    rm analysis.py
-
+    
+    rm output_*
 done
